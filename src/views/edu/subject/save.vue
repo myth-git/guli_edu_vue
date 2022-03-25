4fb2bae62fd0987cc27a11fcad722797709164b6
myth-git <1610680426@qq.com>
2022-03-25 18:19:06 +0800

    课程分类显示

--- src/views/edu/subject/save.vue
+++ src/views/edu/subject/save.vue
@@ -72,7 +72,7 @@ export default {
       });
       //跳转课程分类列表
       //路由跳转
-      //   this.$router.push({ path: "/subject/list" });
+      this.$router.push({ path: "/subject/list" });
     },
     //上传失败
     fileUploadError() {
