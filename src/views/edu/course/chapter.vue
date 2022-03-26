e8422df66e61f1805ef4735630ba71cf3077900b
myth-git <1610680426@qq.com>
2022-03-26 19:11:23 +0800

    增加样式

--- src/views/edu/course/chapter.vue
+++ src/views/edu/course/chapter.vue
@@ -13,13 +13,17 @@
       <el-step title="最终发布" />
     </el-steps>
 
-    <ul>
+    <!-- 章节 -->
+    <ul class="chanpterList">
       <li v-for="chapter in chapterVideoList" :key="chapter.id">
-        {{ chapter.title }}
+        <p>
+          {{ chapter.title }}
+        </p>
 
-        <ul>
+        <!-- 视频 -->
+        <ul class="chanpterList videoList">
           <li v-for="video in chapter.children" :key="video.id">
-            {{ video.title }}
+            <p>{{ video.title }}</p>
           </li>
         </ul>
       </li>
@@ -72,3 +76,42 @@ export default {
   },
 };
 </script>
+<style scoped>
+.chanpterList {
+  position: relative;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.chanpterList li {
+  position: relative;
+}
+.chanpterList p {
+  float: left;
+  font-size: 20px;
+  margin: 10px 0;
+  padding: 10px;
+  height: 70px;
+  line-height: 50px;
+  width: 100%;
+  border: 1px solid #ddd;
+}
+.chanpterList .acts {
+  float: right;
+  font-size: 14px;
+}
+
+.videoList {
+  padding-left: 50px;
+}
+.videoList p {
+  float: left;
+  font-size: 14px;
+  margin: 10px 0;
+  padding: 10px;
+  height: 50px;
+  line-height: 30px;
+  width: 100%;
+  border: 1px dotted #ddd;
+}
+</style>
