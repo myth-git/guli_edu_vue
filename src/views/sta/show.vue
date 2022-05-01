5286c988b3476ec2d8b24a6001845aff7f929c73
myth-git <1610680426@qq.com>
2022-05-01 10:27:28 +0800

    整合后台统计折线图初步整合

--- src/views/sta/show.vue
+++ src/views/sta/show.vue
@@ -1,1 +1,80 @@
-    
+    <template>
+  <div class="app-container">
+    <!--表单-->
+    <el-form :inline="true" class="demo-form-inline">
+      <el-form-item>
+        <el-select v-model="searchObj.type" clearable placeholder="请选择">
+          <el-option label="学员登录数统计" value="login_num" />
+          <el-option label="学员注册数统计" value="register_num" />
+          <el-option label="课程播放数统计" value="video_view_num" />
+          <el-option label="每日课程数统计" value="course_num" />
+        </el-select>
+      </el-form-item>
+
+      <el-form-item>
+        <el-date-picker
+          v-model="searchObj.begin"
+          type="date"
+          placeholder="选择开始日期"
+          value-format="yyyy-MM-dd"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-date-picker
+          v-model="searchObj.end"
+          type="date"
+          placeholder="选择截止日期"
+          value-format="yyyy-MM-dd"
+        />
+      </el-form-item>
+      <el-button
+        :disabled="btnDisabled"
+        type="primary"
+        icon="el-icon-search"
+        @click="showChart()"
+        >查询</el-button
+      >
+    </el-form>
+
+    <div class="chart-container">
+      <div id="chart" class="chart" style="height: 500px; width: 100%" />
+    </div>
+  </div>
+</template>
+<script>
+import echarts from "echarts";
+
+export default {
+  data() {
+    return {
+      searchObj: {},
+      btnDisabled: false,
+    };
+  },
+  methods: {
+    showChart() {
+      // 基于准备好的dom，初始化echarts实例
+      this.chart = echarts.init(document.getElementById("chart"));
+      // console.log(this.chart)
+
+      // 指定图表的配置项和数据
+      var option = {
+        xAxis: {
+          type: "category",
+          data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        },
+        yAxis: {
+          type: "value",
+        },
+        series: [
+          {
+            data: [150, 230, 224, 218, 135, 147, 260],
+            type: "line",
+          },
+        ],
+      };
+      this.chart.setOption(option);
+    },
+  },
+};
+</script>
